fbab6a99bd9fa664c8f0eba1147c3447f0708157
wittnus <wittnus@pm.me>
2025-02-28 16:59:55 +0100

    fitting appears to be working. matrix square root normalization bad for optimizing, trying only normalizing by determinant

--- lib.py
+++ lib.py
@@ -5,6 +5,7 @@ import chex
 from chex import Array
 from jax.random import PRNGKey
 from typing import Tuple
+from itertools import combinations
 
 
 def reject(U: Array, v: Array) -> Array:
@@ -23,10 +24,13 @@ class DetNode:
     @classmethod
     def create(cls, D: int, C: int, key: PRNGKey) -> "DetNode":
         embedding = jax.random.normal(key, (D, C))
-        M = embedding @ embedding.T
+        return cls(embedding=embedding).renormalize()
+
+    def renormalize(self) -> "DetNode":
+        M = self.embedding @ self.embedding.T
         invroot = jnp.linalg.inv(jnp.linalg.cholesky(M))
-        embedding = invroot @ embedding
-        return cls(embedding=embedding)
+        embedding = invroot @ self.embedding
+        return DetNode(embedding=embedding)
 
     @property
     def D(self) -> int:
@@ -37,7 +41,8 @@ class DetNode:
         return self.embedding.shape[1]
 
     def log_prob_unnorm(self, lprobs: Array) -> Array:  # (C,) -> ()
-        M = (self.embedding * jnp.exp(lprobs)) @ self.embedding.T
+        M = (self.embedding * jnp.exp(lprobs)[...,None,:]) @ self.embedding.T
+        M = M + 1e-3*jnp.eye(self.D)
         return jnp.linalg.slogdet(M)[1]
 
     def log_prob(self, lprobs: Array) -> Array:  # (C,) -> ()
@@ -73,29 +78,55 @@ class DetNode:
         return result, final_prob
 
 
+def all_possible_choice_arrays(dnode: DetNode) -> Array:
+    D = dnode.D
+    C = dnode.C
+    tuples = list(combinations(range(C), D))
+    result = jnp.zeros((len(tuples), C), dtype=jnp.bool)
+    for i, t in enumerate(tuples):
+        result = result.at[i, t].set(True)
+    return result
+
+
+def unit_gaussian_log_density(x: Array) -> Array:
+    """include quadratic term as well as normalization"""
+    return -0.5 * jnp.sum(x ** 2, axis=-1) - 0.5 * x.shape[-1] * jnp.log(2 * jnp.pi)
+
+
 @chex.dataclass
 class Model:
     dnode: DetNode
     means: Array  # shape: (C, N)
-    normalizer: Array = None
 
     @classmethod
     def create_with_dnode(cls, dnode: DetNode, N: int, key: PRNGKey) -> "Model":
-        means = jax.random.normal(key, (dnode.C, N)) * 5.
-        return cls(dnode=dnode, means=means).normalize()
+        means = jax.random.normal(key, (dnode.C, N)) * 1.
+        return cls(dnode=dnode, means=means)
 
-    def normalize(self) -> "Model":
+    def renormalize_dnode(self) -> "Model":
+        return Model(dnode=self.dnode.renormalize(), means=self.means)
+
+    def total_variance(self) -> Array:
         embedding = self.dnode.embedding
         means = self.means
         N = self.N
         probs = jnp.einsum("...ij,...ij->...j", embedding, embedding)
         outers = jnp.einsum("...ij,...ik->...ijk", means, means)
         expected_outer = jnp.einsum("...i,...ijk->...jk", probs, outers)
-        total_variance = jnp.eye(N) + expected_outer
-        invroot = jnp.linalg.inv(jnp.linalg.cholesky(total_variance))
-        return Model(dnode=self.dnode, means=means, normalizer=invroot)
+        return jnp.eye(N) + expected_outer
 
-        
+    def unnormalizer(self) -> Array:
+        tvar_logdet = jnp.linalg.slogdet(self.total_variance())[1]
+        factor = jnp.exp(0.5 * tvar_logdet / self.N)
+        return jnp.eye(self.N) * factor
+        return jnp.linalg.cholesky(self.total_variance())
+        return jnp.real(jax.scipy.linalg.sqrtm(self.total_variance()))
+
+    def normalizer(self) -> Array:
+        return jnp.linalg.inv(self.unnormalizer())
+
+    def decode_mean(self, choice: Array) -> Array:
+        return jnp.sum(self.means, axis=-2, where=choice[..., None])
 
     @property
     def N(self) -> int:
@@ -105,8 +136,19 @@ class Model:
     def sample(self, key: PRNGKey) -> Tuple[Array, Array]:
         key1, key2 = jax.random.split(key)
         choice, prob = self.dnode.sample(key1)
-        center = jnp.sum(self.means, axis=0, where=choice[...,None])
+        center = self.decode_mean(choice)
         noise = jax.random.normal(key2, (self.N,))
         sample = center + noise
-        sample = self.normalizer @ sample
+        sample = self.normalizer() @ sample
         return sample, prob
+
+    def exact_log_density(self, x: Array) -> Array:
+        #x = x @ jax.lax.stop_gradient(jax.lax.stop_gradient(self).unnormalizer().T)
+        x = x @ self.unnormalizer().T
+        all_choices = all_possible_choice_arrays(self.dnode)
+        all_means = jax.vmap(self.decode_mean)(all_choices)
+        choice_lprobs = self.dnode.log_prob(jnp.log(all_choices))
+        diffs = x[...,None,:] - all_means
+        log_densities = unit_gaussian_log_density(diffs)
+        tvar_logdet = jnp.linalg.slogdet(self.total_variance())[1]
+        return jax.scipy.special.logsumexp(log_densities + choice_lprobs, axis=-1) + 0.5 * tvar_logdet
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from typing import Tuple
 from jax.random import PRNGKey
 from itertools import combinations
 from tqdm import tqdm, trange
+from jax.tree_util import tree_map
 
 import matplotlib
 from matplotlib import pyplot as plt
@@ -34,50 +35,116 @@ def choice_prob(dnode: DetNode, choice: Array) -> Array:
     return jnp.exp(dnode.log_prob(jnp.log(choice.astype(jnp.float32))))
 
 
-def main():
-    init_key = PRNGKey(0)
-    D = 2
-    C = 4
-    N = 2
-    dnode = DetNode.create(D, C, init_key)
-    #manual_embedding = jnp.array([one_hot(0,D), one_hot(0,D), one_hot(1,D), one_hot(1,D)]).T
-    #dnode = DetNode(embedding=manual_embedding)
+def sample_model(model: Model, key: PRNGKey, count: int) -> Tuple[Array, Array]:
+    samples = list()
+    for subkey in jax.random.split(key, count):
+        sample, prob = model.sample(subkey)
+        samples.append(sample)
+    return np.array(samples)
 
-    sample_key = PRNGKey(3)
-    sample, prob = dnode.sample(sample_key)
-    print(sample, prob)
+def check_cond_lprob(dnode: DetNode, key: PRNGKey) -> None:
+    sample, prob = dnode.sample(key)
     cond_lprob = jnp.log(prob)
     overall_lprob = dnode.log_prob(jnp.log(sample.astype(jnp.float32)))
     print(f"cond_lprob: {cond_lprob}, overall_lprob: {overall_lprob}")
     assert cond_lprob <= overall_lprob
 
-    all_choices = all_possible_choices(D, C)
+def compare_probs(dnode: DetNode, key: PRNGKey) -> None:
+    all_choices = all_possible_choices(dnode.D, dnode.C)
     overall_probs = {
-        choice: choice_prob(dnode, choice_as_array(choice, C)) for choice in all_choices
+        choice: choice_prob(dnode, choice_as_array(choice, dnode.C)) for choice in all_choices
     }
     for choice, prob in overall_probs.items():
-        print(f"{choice_as_array(choice, C)}: {prob}")
+        print(f"{choice_as_array(choice, dnode.C)}: {prob}")
     print(f"sum of probs: {sum(overall_probs.values())}")
 
     counts = dict()
     num_samples = 1000
-    for subkey in tqdm(jax.random.split(sample_key, num_samples)):
+    for subkey in tqdm(jax.random.split(key, num_samples)):
         sample, prob = dnode.sample(subkey)
         choice = choice_array_as_tuple(sample)
         counts[choice] = counts.get(choice, 0) + 1
     for choice in all_choices:
-        print(f"{choice_as_array(choice, C)}: {counts.get(choice, 0) / num_samples}")
+        print(f"{choice_as_array(choice, dnode.C)}: {counts.get(choice, 0) / num_samples}")
 
-    model_init_key = PRNGKey(4)
-    model = Model.create_with_dnode(dnode, N, model_init_key)
-    model_sample_key = PRNGKey(5)
+def sample_model(model: Model, key: PRNGKey, count: int) -> Array:
     samples = list()
-    for subkey in tqdm(jax.random.split(model_sample_key, 1000)):
+    for subkey in jax.random.split(key, count):
         sample, prob = model.sample(subkey)
         samples.append(sample)
-    samples = np.array(samples)
+    return np.array(samples)
+
+def visualize_model_samples(model: Model, key: PRNGKey) -> None:
+    samples = np.array(sample_model(model, key, 1000))
     plt.scatter(samples[:, 0], samples[:, 1])
     plt.show()
+
+def compare_models_at_samples_from1(model1: Model, model2: Model, key: PRNGKey, count: int) -> None:
+    samples = sample_model(model1, key, count)
+    lprobs1 = model1.exact_log_density(samples)
+    lprobs2 = model2.exact_log_density(samples)
+    for s, p1, p2 in zip(samples, lprobs1, lprobs2):
+        print(f"{s} {p1:.3f} {p2:.3f}")
+    KL = np.mean(lprobs1 - lprobs2)
+    print(f"KL: {KL}")
+
+def estimate_KL(model1: Model, model2: Model, key: PRNGKey, count: int) -> float:
+    samples = sample_model(model1, key, count)
+    lprobs1 = model1.exact_log_density(samples)
+    lprobs2 = model2.exact_log_density(samples)
+    KL = np.mean(lprobs1 - lprobs2)
+    return KL
+
+def make_model(D, C, N, key):
+    key1, key2 = jax.random.split(key)
+    dnode = DetNode.create(D, C, key1)
+    model = Model.create_with_dnode(dnode, N, key2)
+    return model
+
+def model_nll(model, data):
+    return -model.exact_log_density(data).mean()
+
+def fit_model(model, data):
+    @jax.jit
+    @jax.value_and_grad
+    def loss_grad(model, data):
+        return model_nll(model, data)
+    for i in range(10000):
+        loss, grad = loss_grad(model, data)
+        if i % 100 == 0:
+            print(f"loss: {loss}")
+        model = tree_map(lambda p, g: p - 1e-2 * g, model, grad).renormalize_dnode()
+    return model
+
+
+def main():
+    init_key = PRNGKey(1)
+    D = 2
+    C = 4
+    N = 2
+    dnode = DetNode.create(D, C, init_key)
+    manual_embedding = jnp.array([one_hot(0,D), one_hot(0,D), one_hot(1,D), one_hot(1,D)]).T
+    dnode = DetNode(embedding=manual_embedding)
+
+    check_cond_lprob(dnode, PRNGKey(3))
+    compare_probs(dnode, PRNGKey(2))
+
+    model1 = Model.create_with_dnode(dnode, N, PRNGKey(4))
+    #visualize_model_samples(model1, PRNGKey(5))
+    model1 = make_model(D, C, N, PRNGKey(4))
+    model2 = make_model(D+1, C+2, N, PRNGKey(6))
+    compare_models_at_samples_from1(model1, model2, PRNGKey(6), 10)
+
+    data_from_model1 = sample_model(model1, PRNGKey(7), 1000)
+    model2 = fit_model(model2, data_from_model1)
+    nll1 = model_nll(model1, data_from_model1)
+    nll2 = model_nll(model2, data_from_model1)
+    print(f"nll1: {nll1}, nll2: {nll2}")
+    compare_models_at_samples_from1(model1, model2, PRNGKey(8), 10)
+
+    KL = estimate_KL(model1, model2, PRNGKey(9), 1000)
+    print(f"Estimated KL: {KL}")
+
 
 
 if __name__ == "__main__":
